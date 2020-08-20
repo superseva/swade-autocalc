b2f4f805c8db5583e3c9c501a4c2e8e37297d4dd
superseva <superseva@gmail.com>
2020-08-20 18:14:34 +0200

    Parry Calculation
    Only Calculating Parry depending on Fighting and Modifier

--- scripts/swade-sheet-modifiers.js
+++ scripts/swade-sheet-modifiers.js
@@ -13,9 +13,11 @@ class SwadeSheetExtender extends FormApplication{
   }
 
   static updateActorToughness(actor){
+	let _actor = game.actors.get(actor._id);
     let _toughnessBase = actor.data.data.attributes.vigor.die.sides/2 + 2;
-    let _toughnessPlusModiffier = _toughnessBase + actor.data.data.stats.toughness.armor + actor.data.data.stats.toughness.modifier + actor.data.data.stats.size;
-    let _actor = game.actors.get(actor._id);
+	let _actArm = _actor.calcArmor();
+    let _toughnessPlusModiffier = _toughnessBase + _actArm + actor.data.data.stats.toughness.modifier + actor.data.data.stats.size;
+    
     _actor.update({ 'data.stats.toughness.value': _toughnessPlusModiffier });
   }
 
@@ -44,7 +46,7 @@ class SwadeSheetExtender extends FormApplication{
 }
 /* Adding Hooks */
 //Hooks.on('init', () => {CONFIG.debug.hooks = true;});
-
+/*
 Hooks.on('renderActorSheet', (app, html, data) => {
     let parryField = html.find('#parry');
     let parryModifierValue = data.data.stats.parry.modifier;
@@ -58,12 +60,13 @@ Hooks.on('renderActorSheet', (app, html, data) => {
     let armorField = $(`<input id="toughness-armor" name="data.stats.toughness.armor" value="${armorValue}" type="text" placeholder="0" data-dtype="Number"><input id="toughness-modifier" name="data.stats.toughness.modifier" value="${toughnessModifierValue}" type="text" placeholder="0" data-dtype="Number">`);
      armorField.insertAfter(toughnessField);
 });
+*/
 
 Hooks.on('updateActor', (actor, data, diff) => {
     // UPDATE TOUGHNESS BY CHANGING ARMOR OR MODIFIER VALUE
-    if(SwadeSheetExtender.getVar(data, 'data.stats.toughness.armor')!=undefined || SwadeSheetExtender.getVar(data, 'data.stats.toughness.modifier')!=undefined || SwadeSheetExtender.getVar(data, 'data.attributes.vigor.die')!=undefined|| SwadeSheetExtender.getVar(data, 'data.stats.size')!=undefined){
+    /*if(SwadeSheetExtender.getVar(data, 'data.stats.toughness.armor')!=undefined || SwadeSheetExtender.getVar(data, 'data.stats.toughness.modifier')!=undefined || SwadeSheetExtender.getVar(data, 'data.attributes.vigor.die')!=undefined|| SwadeSheetExtender.getVar(data, 'data.stats.size')!=undefined){
       SwadeSheetExtender.updateActorToughness(actor);
-    }
+    }*/
 
     // UPDATE PARRY BY CHANGING MODIFIER VALUE
     if(SwadeSheetExtender.getVar(data, 'data.stats.parry.modifier')!=undefined){
@@ -76,6 +79,10 @@ Hooks.on('updateOwnedItem', (actor, item, data) => {
   if(item.name == 'Fighting' && item.type=='skill'){
     SwadeSheetExtender.updateActorParry(actor, item.data.die.sides);
   }
+  //UPDATE TOUGHNESS WHEN EQUIPING ARMOR
+  /*if(item.type=="armor" && item.data.locations.torso){
+	  SwadeSheetExtender.updateActorToughness(actor);
+  }*/
 });
 
 Hooks.on('createOwnedItem', (actor, item, data) =>{
